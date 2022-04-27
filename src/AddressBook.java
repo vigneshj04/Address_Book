9c186620e816ed911592fab787ebad2af44410c4
vigneshj04 <vigneshjmax@gmail.com>
2022-04-27 11:20:40 +0530

    Added: Creating Contacts

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,2 +1,3 @@
-public class AddressBook {
+public class AddressBook{
+
 }
--- src/Contacts.java
+++ src/Contacts.java
@@ -1,2 +1,13 @@
-public class Contacts {
-}
+
+    public class Contacts {
+
+        String firstname;
+        String lastname;
+        String address;
+        String city;
+        String zip;
+        String state;
+        String phone_number;
+        String email;
+ }
+
