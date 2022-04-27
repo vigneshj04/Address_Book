bfa7926d73927776b3128cf235d6a03a9f09840b
vigneshj04 <vigneshjmax@gmail.com>
2022-04-27 23:29:50 +0530

    Added: Search Option

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,7 +1,5 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class AddressBook {
@@ -17,6 +15,77 @@ public class AddressBook {
             }
         }
         return false;
+    }
+
+
+    public static void addNewAddressBook() {
+        System.out.println("Enter the Address Book Name :");
+        String addressBookName = sc.next();
+        if (addressBookMap.containsKey(addressBookName)) {
+            System.out.println("Entered AddressBook is Already Available");
+        } else {
+            AddressBook addressBook = new AddressBook();
+            addressBookMap.put(addressBookName, addressBook);
+        }
+
+    }
+
+    // method to search contact by name
+    public List<Contacts> searchByName(String name) {
+        return list.stream().filter(person -> person.getFirstName().equalsIgnoreCase(name))
+                .collect(Collectors.toList());
+    }
+
+    // by city
+    public List<Contacts> searchByCity(String city) {
+        return list.stream().filter(person -> person.getCity().equalsIgnoreCase(city))
+                .collect(Collectors.toList());
+    }
+
+    // by state
+    public List<Contacts> searchByState(String state) {
+        return list.stream().filter(person -> person.getState().equalsIgnoreCase(state))
+                .collect(Collectors.toList());
+    }
+
+
+    public void searchByOptions() {
+        Main addressBook = new Main();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. By name");
+        System.out.println("2. By city");
+        System.out.println("3. By state");
+        System.out.println("Your choice: ");
+        int choice = sc.nextInt();
+        sc.nextLine();
+        switch (choice) {
+            case 1:
+                System.out.println("Enter name: ");
+                String name = sc.nextLine();
+                list.forEach(book -> searchByName(name).forEach(System.out::println));
+                break;
+            case 2:
+                System.out.println("Enter city: ");
+                String city = sc.nextLine();
+                list.forEach(book -> searchByCity(city).forEach(System.out::println));
+                break;
+            case 3:
+                System.out.println("Enter state: ");
+                String state = sc.nextLine();
+                list.forEach(book -> searchByState(state).forEach(System.out::println));
+                break;
+            default:
+                System.out.println("Invalid Choice");
+        }
+    }
+
+
+    public static void displayAddressBooks() {
+        for (Map.Entry<String, AddressBook> entry : addressBookMap.entrySet()) {
+            System.out.println(entry.getKey());
+            entry.getValue().showContacts();
+
+        }
     }
 
     public void addContacts() {
@@ -103,7 +172,6 @@ public class AddressBook {
 
     public void deleteContact() {
 
-
         System.out.println("Enter the first name");
         String firstName = sc.next();
 
@@ -121,15 +189,33 @@ public class AddressBook {
         }
     }
 
-    public static void addNewAddressBook() {
-        System.out.println("Enter the Address Book Name :");
-        String addressBookName = sc.next();
-        if (addressBookMap.containsKey(addressBookName)) {
-            System.out.println("Entered AddressBook is Already Available");
-        } else {
-            AddressBook addressBook = new AddressBook();
-            addressBookMap.put(addressBookName, addressBook);
-        }
 
+
+
+    public void chooseoptionforcontact() {
+        int choice;
+        do {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Kindly Enter choice \n 1. Add new Contact \n 2. Edit Contact details \n 3. Display details  \n 4 Delete Contact \n 5. Quit");
+            choice = scan.nextInt();
+            switch (choice) {
+                case 1:
+                    addContacts();
+                    break;
+                case 2:
+                    editContacts();
+                    break;
+                case 3:
+                    showContacts();
+                    break;
+                case 4:
+                    deleteContact();
+                    break;
+                case 5:
+                    System.out.println("Thank you.");
+                    break;
+            }
+        } while (choice != 5);
     }
 }
+
